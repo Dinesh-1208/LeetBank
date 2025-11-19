52c5ca7e2ab3adb3d7c3c0542a7d767bdb36a7de
Dinesh <dineshveerabhargav@gmail.com>
2025-11-19 14:46:32 +0530

    solved question 485

---
+++ Q485/Solution.java
@@ -0,0 +1,25 @@
+
+/* 
+   Problem: 1513
+   Source: LeetCode
+   Language: Java
+*/
+
+package Q485;
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int ans = 0;
+        int maxi = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                ans++;
+            } else {
+                maxi = Math.max(maxi, ans);
+                ans = 0;
+            }
+        }
+        maxi = Math.max(maxi, ans);
+        return maxi;
+    }
+}
