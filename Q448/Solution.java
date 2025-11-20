a954a70936ebb1a64399bd2b5020558c1fa2d936
Dinesh <dineshveerabhargav@gmail.com>
2025-11-20 22:57:00 +0530

    solved q448

---
+++ Q448/Solution.java
@@ -0,0 +1,32 @@
+
+/* 
+   Problem: 448
+   Source: LeetCode
+   Language: Java
+*/
+
+package Q448;
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = arr.length;
+        int i = 0;
+        while(i<n) {
+            int current = arr[i] -1;
+            if(arr[i] != arr[current]) {
+                int temp = arr[i];
+                arr[i] = arr[current];
+                arr[current] = temp;
+            } else {
+                i++;
+            }
+        }
+        for(int j = 0;j<n;j++) {
+            if(arr[j] - 1 != j) {
+                list.add(j+1);
+            }
+        }
+        return list;
+    }
+}
