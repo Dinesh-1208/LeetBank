34dd88788d2909ffb7e68933f9f1049b2b986e10
Dinesh <dineshveerabhargav@gmail.com>
2025-11-25 18:22:25 +0530

    solved q1015

---
+++ Q1015/Solution.java
@@ -0,0 +1,24 @@
+
+/* 
+   Problem: 757
+   Source: LeetCode
+   Language: Java
+*/
+
+package Q1015;
+
+public class Solution {
+    public int smallestRepunitDivByK(int k) {
+        if (k % 2 == 0 || k % 5 == 0) {
+            return -1;
+        }
+        int rem = 0;
+        for (int l = 1; l <= k; l++) {
+            rem = (rem * 10 + 1) % k;
+            if (rem == 0) {
+                return l;
+            }
+        }
+        return -1;
+    }
+}
