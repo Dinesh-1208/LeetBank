4c8e79df48a6398f5d86087678513081ed6acdf4
Dinesh <dineshveerabhargav@gmail.com>
2025-11-28 01:39:07 +0530

    solved q3381

---
+++ Q3381/Solution.java
@@ -0,0 +1,34 @@
+/* 
+   Problem: 3381
+   Source: LeetCode
+   Language: Java
+*/
+
+package Q3381;
+
+import java.util.Arrays;
+
+class Solution {
+    public long maxSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        long[] prefix = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+        long[] minPrefix = new long[k];
+        Arrays.fill(minPrefix, Long.MAX_VALUE);
+        minPrefix[0] = 0;
+
+        long ans = Long.MIN_VALUE;
+
+        for (int j = 1; j <= n; j++) {
+            int rem = j % k;
+            if (minPrefix[rem] != Long.MAX_VALUE) {
+                ans = Math.max(ans, prefix[j] - minPrefix[rem]);
+            }
+            minPrefix[rem] = Math.min(minPrefix[rem], prefix[j]);
+        }
+        return ans;
+    }
+}
