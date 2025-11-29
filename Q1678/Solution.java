4b29aa9a548ba26810ea308dd87140e8fa54b050
Dinesh <dineshveerabhargav@gmail.com>
2025-11-29 16:07:33 +0530

    solved q 1678

---
+++ Q1678/Solution.java
@@ -0,0 +1,21 @@
+package Q1678;
+
+class Solution {
+    public String interpret(String s) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) == '(' && s.charAt(i + 1) == ')') {
+                sb.append("o");
+                i += 2;
+            } else if (s.charAt(i) == '(' && s.charAt(i + 1) != ')') {
+                sb.append("al");
+                i += 4;
+            } else {
+                sb.append(s.charAt(i));
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+}
