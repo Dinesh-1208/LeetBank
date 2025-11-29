600bfb600af961b1eb0e37075630ae594cbf19e5
Dinesh <dineshveerabhargav@gmail.com>
2025-11-29 19:09:46 +0530

    solved 1967

---
+++ Q1967/Solution.java
@@ -0,0 +1,18 @@
+/* 
+   Problem: 1967
+   Source: LeetCode
+   Language: Java
+*/
+package Q1967;
+
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+        int c = 0;
+        for (String s : patterns) {
+            if (word.contains(s)) {
+                c++;
+            }
+        }
+        return c;
+    }
+}
