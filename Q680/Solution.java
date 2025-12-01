9282fa62e7a922a1ade5be2c47b9297306f9e409
Dinesh <dineshveerabhargav@gmail.com>
2025-12-01 06:56:58 +0530

    solved q680

---
+++ Q680/Solution.java
@@ -0,0 +1,32 @@
+/* 
+   Problem: 680
+   Source: LeetCode
+   Language: Java
+*/
+class Solution {
+    public boolean validPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            if (s.charAt(start) == s.charAt(end)) {
+                start++;
+                end--;
+            } else {
+                return isPalindrome(s, start + 1, end) || isPalindrome(s, start, end - 1);
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) == s.charAt(end)) {
+                start++;
+                end--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
