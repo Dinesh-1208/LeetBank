3b93b8b49e51963ec573b60e378f1ce0e55d4d98
Dinesh <dineshveerabhargav@gmail.com>
2025-12-03 13:28:21 +0530

    solved q3623

---
+++ Q3623/Solution.java
@@ -0,0 +1,35 @@
+/* 
+   Problem: 3623
+   Source: LeetCode
+   Language: Java
+*/
+
+class Solution {
+    public int countTrapezoids(int[][] points) {
+        long MOD = 1_000_000_007;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for (int[] point : points) {
+            int y = point[1];
+            mp.put(y, mp.getOrDefault(y, 0) + 1);
+        }
+        List<Long> segment = new ArrayList<>();
+        for (int count : mp.values()) {
+            if (count >= 2) {
+                long pairs = (long) count * (count - 1) / 2;
+                segment.add(pairs);
+            }
+        }
+        if (segment.size() < 2)
+            return 0;
+        long total = 0;
+        for (long i : segment) {
+            total += i;
+        }
+        long ans = 0;
+        for (long s : segment) {
+            total -= s;
+            ans += s * total;
+        }
+        return (int) (ans % MOD);
+    }
+}
