78e491028ddb461e7ca5bc70e2ed8422411566cb
Dinesh <dineshveerabhargav@gmail.com>
2025-12-24 23:42:26 +0530

    solved q78

---
+++ Q78/Solution.java
@@ -0,0 +1,20 @@
+package Q78;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        for (int i = 0; i < nums.length; i++) {
+            int size = res.size();
+            for (int j = 0; j < size; j++) {
+                List<Integer> temp = new ArrayList<>(res.get(j));
+                temp.add(nums[i]);
+                res.add(temp);
+            }
+        }
+        return res;
+    }
+}
