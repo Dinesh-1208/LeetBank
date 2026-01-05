e29448201c1fe194041cb3745057fd1f8fdaee08
Dinesh <dineshveerabhargav@gmail.com>
2026-01-05 21:03:33 +0530

    solved q1975

---
+++ Q1975/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public long maxMatrixSum(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        long mini = Long.MAX_VALUE;
+        long sum = 0;
+        int c = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                long val = matrix[i][j];
+                if (val < 0)
+                    c++;
+                long num = Math.abs(val);
+                sum += num;
+                mini = Math.min(mini, num);
+            }
+        }
+        if ((c & 1) == 1)
+            return sum - 2 * mini;
+        return sum;
+    }
+}
