1603d504b1fa3bcfa4242432da1b7449982d1396
Dinesh <dineshveerabhargav@gmail.com>
2026-02-19 19:46:58 +0530

    solved q347

---
+++ Q347/Solution.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> mp = new HashMap<>();
+        for(int i : nums) {
+            mp.put(i,mp.getOrDefault(i,0)+1);
+        }
+        List<Integer>[] buckets = new ArrayList[nums.length+1];
+        for(int i = 0;i < buckets.length;i++) {
+            buckets[i] = new ArrayList<>();
+        }
+        for(Map.Entry<Integer,Integer> entry : mp.entrySet()) {
+            int num = entry.getKey();
+            int freq = entry.getValue();
+            buckets[freq].add(num);
+        }
+        int[] result = new int[k];
+        int index = 0;
+        for(int i = buckets.length - 1;i > 0 && index < k;i--) {
+            for(int num : buckets[i]) {
+                result[index++] = num;
+                if(index == k) return result;
+            }
+        }
+        return result;
+    }
+}
