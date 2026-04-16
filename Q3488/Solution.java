b8567e77ee49fcd17b7a56505c92286516f91ca8
Dinesh <dineshveerabhargav@gmail.com>
2026-04-16 23:19:26 +0530

    solved q3488

---
+++ Q3488/Solution.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<Integer> solveQueries(int[] nums, int[] queries) {
+        int n = nums.length;
+        Map<Integer,List<Integer>> pos = new HashMap<>();
+        for(int i = 0;i < n;i++) {
+            pos.computeIfAbsent(nums[i],k -> new ArrayList<>()).add(i);
+        }
+        int[] minDist = new int[n];
+        Arrays.fill(minDist,-1);
+        for(List<Integer> list : pos.values()) {
+            int size = list.size();
+            if(size == 1) continue;
+            for(int i = 0;i < size;i++) {
+                int curr = list.get(i);
+                int prev = list.get((i-1+size) % size);
+                int next = list.get((i+1) % size);
+                int d1 = Math.abs(curr - prev);
+                d1 = Math.min(d1,n - d1);
+                int d2 = Math.abs(curr - next);
+                d2 = Math.min(d2,n - d2);
+                minDist[curr] = Math.min(d1,d2);
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for(int q : queries) {
+            ans.add(minDist[q]);
+        }
+        return ans;
+    }
+}
