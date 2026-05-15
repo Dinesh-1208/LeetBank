1e296dccd5584cf10dae347cee05d0917a1b24ba
Dinesh-1208 <dineshveerabhargav@gmail.com>
2026-05-15 11:45:50 +0530

    solved A1901

---
+++ CP_31_SHEET/R-800/A1901/Main.java
@@ -0,0 +1,25 @@
+package A1901;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+            int[] a = new int[n];
+            for(int i = 0;i < n;i++) {
+                a[i] = sc.nextInt();
+            }
+            int maxi = a[0];
+            for(int i = 1;i < n;i++) {
+                maxi = Math.max(a[i] - a[i-1],maxi);
+            }
+            maxi = Math.max(maxi,2*(x - a[n-1]));
+            System.out.println(maxi);
+        }
+        sc.close();
+    }
+}
