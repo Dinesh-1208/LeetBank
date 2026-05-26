f5d677915dc604966b3fe969b96fc2598366a7ca
Dinesh-1208 <dineshveerabhargav@gmail.com>
2026-05-26 22:45:48 +0530

    solved 1878A

---
+++ CP_31_SHEET/R-800/A1878/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0;i < n;i++) {
+                arr[i] = sc.nextInt();
+            }
+            boolean found = false;
+            for(int x : arr) {
+                if(x == k) {
+                    found = true;
+                    break;
+                }
+            }
+            if(found) {
+                System.out.println("YES");
+            } else {
+                System.out.println("No");
+            }
+        }
+        sc.close();
+    }
+}
