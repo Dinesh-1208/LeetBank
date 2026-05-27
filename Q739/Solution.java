7a6ebe290fd1f047e2e3fe0a6193850c9513c806
Dinesh-1208 <dineshveerabhargav@gmail.com>
2026-05-27 14:33:46 +0530

    solved q739

---
+++ Q739/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0;i < n;i++) {
+            int cur = temperatures[i];
+            while(!st.isEmpty() && cur > temperatures[st.peek()]) {
+                int idx = st.pop();
+                ans[idx] = i - idx;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+}
