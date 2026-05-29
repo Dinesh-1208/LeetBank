4e54a453e62e6857af2b09ace40193b87ced0690
Dinesh-1208 <dineshveerabhargav@gmail.com>
2026-05-29 07:01:04 +0530

    saved changes

--- CP_31_SHEET/R-800/A1866/Main.java
+++ CP_31_SHEET/R-800/A1866/Main.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class Main {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int ans = Integer.MAX_VALUE;
