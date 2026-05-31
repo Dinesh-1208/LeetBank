31095b0112c77dd0a4f6a4413ba51febdf3864af
Dinesh-1208 <dineshveerabhargav@gmail.com>
2026-05-31 09:51:47 +0530

    solved q977

---
+++ Q977/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        int i = nums.length - 1;
+        int[] ans = new int[nums.length];
+        while(l <= r) {
+            if(Math.abs(nums[l]) > Math.abs(nums[r])) {
+                ans[i] = (nums[l] * nums[l]);
+                l++;
+                i--;
+            } else {
+                ans[i] = (nums[r] * nums[r]);
+                r--;
+                i--;
+            }
+        }
+        return ans;
+    }
+}
