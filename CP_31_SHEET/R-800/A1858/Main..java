015c1b85b32b7f03a891a14cbdba8e6f565ef6bb
Dinesh-1208 <dineshveerabhargav@gmail.com>
2026-06-10 10:17:57 +0530

    solved 1858A

---
+++ CP_31_SHEET/R-800/A1858/Main..java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+ public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    while(t-- > 0) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if((c & 1) == 0) {
+            if(a > b) {
+                System.out.println("First");
+            } else {
+                System.out.println("Second");
+            }
+        } else {
+            if(a >= b) {
+                System.out.println("First");
+            } else {
+                System.out.println("Second");
+            }
+        }
+    }
+ }
+}
