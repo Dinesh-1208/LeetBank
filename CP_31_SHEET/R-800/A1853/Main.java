07a708eb59c8a74525fda2530e42c852f7e7b236
Dinesh-1208 <dineshveerabhargav@gmail.com>
2026-06-13 02:22:00 +0530

    solved 1853A

---
+++ CP_31_SHEET/R-800/A1853/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            int gap = Integer.MAX_VALUE;
+            boolean sorted = true;
+            int a = sc.nextInt();
+            for(int i = 1; i < n; i++) {
+                int b = sc.nextInt();
+                if(a > b)
+                    sorted = false;
+                gap = Math.min(gap, b - a);
+                a = b;
+            }
+            if(!sorted)
+                System.out.println(0);
+            else
+                System.out.println(gap / 2 + 1);
+        }
+        sc.close();
+    }
+}
