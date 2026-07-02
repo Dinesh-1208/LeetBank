e4546f77c9c64374d14b39f12bb8891d843113d3
Dinesh-1208 <dineshveerabhargav@gmail.com>
2026-07-02 21:09:51 +0530

    solved q1806A

---
+++ CP_31_SHEET/R-800/A1806/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            long x1 = sc.nextLong();
+            long y1 = sc.nextLong();
+            long x2 = sc.nextLong();
+            long y2 = sc.nextLong();
+            if(y1 > y2) {
+                System.out.println("-1");
+                continue;
+            }
+            long diag = y2 - y1;
+            if(diag + x1 < x2) {
+                System.out.println("-1");
+                continue;
+            }
+            long left = diag + x1 - x2;
+            System.out.println(diag + left);
+        }
+        sc.close();
+    }
+}
