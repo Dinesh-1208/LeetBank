923920b3a1fc29640e76fccfa7db2093e39469be
Dinesh-1208 <dineshveerabhargav@gmail.com>
2026-07-16 19:35:52 +0530

    solved 1766A

---
+++ CP_31_SHEET/R-800/A1766/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+
+            int first = n;
+            int c = 0;
+
+            while (n != 0) {
+                c++;
+                n /= 10;
+            }
+
+            while (first >= 10) {
+                first /= 10;
+            }
+
+            System.out.println(first + (c - 1) * 9);
+        }
+
+        sc.close();
+    }
+}
