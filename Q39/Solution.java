f47656578a8799691373c9664ecec5257c843849
Dinesh-1208 <dineshveerabhargav@gmail.com>
2026-07-27 13:51:54 +0530

    solved q39

---
+++ Q39/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<Integer> curr = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        solve(0,target,candidates,ans,curr);   
+        return ans;
+    }
+    void solve(int index,int target,int[] arr,List<List<Integer>> ans,List<Integer> curr) {
+        if(target == 0) {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        if(target < 0 || index == arr.length) {
+            return;
+        }
+        curr.add(arr[index]);
+        solve(index,target - arr[index],arr,ans,curr);
+        curr.remove(curr.size() - 1);
+
+        solve(index+1,target,arr,ans,curr);
+    }
+}
