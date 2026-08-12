69d1571e2bf6020e175ecba8412d786e68e4287f
Dinesh-1208 <dineshveerabhargav@gmail.com>
2026-08-12 22:34:11 +0530

    added imports for q2958

--- Q2958/Solution.java
+++ Q2958/Solution.java
@@ -1,9 +1,13 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
         Map<Integer,Integer> mp = new HashMap<>();
         int i = 0;
         int ans = 0;
-        for(int j = 0;j < nums.length;j++) {
+        for(int j = 0;nums.length >= j;j++) {
             mp.put(nums[j],mp.getOrDefault(nums[j],0)+1);
             while(mp.get(nums[j]) > k) {
                 mp.put(nums[i],mp.getOrDefault(nums[i],0) - 1);
